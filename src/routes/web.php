69e11aadd440ae0566683bdfbc0ec9e4fd2f3f39
Daisuke-Kaneko07 <dk.chosenone@gmail.com>
2018-12-27 23:24:42 +0900

    laravelチュートリアル ルートとビューの準備

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,5 +12,22 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+Route::post('/task', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+});
+
+Route::delete('/task/{id}', function ($id) {
+    //
 });
